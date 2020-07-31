83c3cad5e29b187f34f0322dd909d4303a593b6c
Ana Caroline Abreu <anacarolinee.as@gmail.com>
2020-07-30 23:55:11 -0300

    mod 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -6,12 +6,12 @@ import Button from '../Button';
 function Menu() {
     return ( 
         <nav className="Menu">
-        <a href="/">
-            <img className="Logo" src={Logo} alt="Gameflix logo"></img>
-        </a>
-        <Button as="a" className="ButtonLink" href="/">
-            Novo vídeo
-        </Button>
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Gameflix logo"></img>
+            </a>
+            <Button as="a" className="ButtonLink" href="/">
+                Novo vídeo
+            </Button>
         </nav>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo() {
+  return (
+    <div>
+      Página de Cadastro de Vídeo
+    </div>
+
+  );
+}
+
+//BroserRouter apresenta as rotas dos navegadores
+//Switch: como se fosse um if, adicionar cada rota referente a página
+//Route, especifica o path (caminho) e o componente
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch> 
+    <Route path="/cadastro/video" component={CadastroVideo}/>
+    <Route path="/" component={App} exact/>
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
